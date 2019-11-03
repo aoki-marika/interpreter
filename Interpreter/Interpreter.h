afb24a5558c2a109cdd71f8a7ff0650aac887675
marika <marika@waifu.club>
2019-11-03 08:30:00 -0400

    Cleanup the templates.

--- Interpreter/Interpreter.h
+++ Interpreter/Interpreter.h
@@ -8,12 +8,5 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for Interpreter.
 FOUNDATION_EXPORT double InterpreterVersionNumber;
-
-//! Project version string for Interpreter.
 FOUNDATION_EXPORT const unsigned char InterpreterVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Interpreter/PublicHeader.h>
-
-
