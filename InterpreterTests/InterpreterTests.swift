5103a392a900c1ff69d43807d308663ebc183b28
marika <marika@waifu.club>
2019-11-03 09:37:42 -0400

    Add integer addition tests.

--- InterpreterTests/InterpreterTests.swift
+++ InterpreterTests/InterpreterTests.swift
@@ -10,4 +10,17 @@ import XCTest
 @testable import Interpreter
 
 class InterpreterTests: XCTestCase {
+
+    func assertAddIntegers(lhs: Int, rhs: Int) {
+        let interpreter = Interpreter(text: "\(lhs)+\(rhs)")
+        XCTAssertNoThrow(try interpreter.evaluate())
+        XCTAssertEqual(try? interpreter.evaluate() as? Int, lhs + rhs)
+    }
+
+    func testAddIntegers() {
+        assertAddIntegers(lhs: 5, rhs: 7)
+        assertAddIntegers(lhs: 1, rhs: 3)
+        assertAddIntegers(lhs: 9, rhs: 6)
+        assertAddIntegers(lhs: 8, rhs: 8)
+    }
 }
