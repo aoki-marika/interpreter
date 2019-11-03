656a45b252db198023c2f32befb0955aab4a6adf
marika <marika@waifu.club>
2019-11-03 08:29:10 -0400

    Initial commit.

---
+++ Interpreter/Interpreter.h
@@ -0,0 +1,19 @@
+//
+//  Interpreter.h
+//  Interpreter
+//
+//  Created by Marika on 2019-11-03.
+//  Copyright © 2019 Marika. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Interpreter.
+FOUNDATION_EXPORT double InterpreterVersionNumber;
+
+//! Project version string for Interpreter.
+FOUNDATION_EXPORT const unsigned char InterpreterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Interpreter/PublicHeader.h>
+
+
